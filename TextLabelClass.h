9400e65a6034664413b40a3e00ac7a7ed06310d1
secsome <302702960@qq.com>
2022-09-23 18:00:54 +0800

    Update MessageListClass

--- TextLabelClass.h
+++ TextLabelClass.h
@@ -10,7 +10,7 @@ public:
 	//Statics
 
 	//Constructors
-	TextLabelClass(wchar_t* pText, int X, int Y, DWORD dwColor, TextPrintType style) noexcept
+	TextLabelClass(wchar_t* pText, int X, int Y, int ColorSchemeIndex, TextPrintType style) noexcept
 		: GadgetClass(noinit_t()) { JMP_THIS(0x72A440); }
 protected:
 	explicit __forceinline TextLabelClass(noinit_t)  noexcept
@@ -25,7 +25,7 @@ public:
 	void* UserData2;
 	DWORD Style;
 	wchar_t* Text;
-	DWORD Color;
+	int ColorSchemeIndex;
 	DWORD PixWidth;
 	DWORD anim_dword3C;
 	bool SkipDraw;
